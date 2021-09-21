b571b388304839d62b30d14858aa386afc723727
Ferlern <seeker1233@ukr.net>
2021-09-21 13:22:21 +0300

    rewrite sync

--- ext/sync/sync.py
+++ ext/sync/sync.py
@@ -1,64 +1,98 @@
 import logging
-import sys
-import traceback
 
 import discord
-from core import Personal_voice, User_info, User_roles
+from core import Member_data_controller, Personal_voice, User_roles
 from discord.ext import commands
 from discord.utils import get
 from main import SEBot
+from utils.utils import DefaultEmbed
+
+from ..utils.checks import is_owner
 
 logger = logging.getLogger('Arctic')
+added = set[int]
+outdated = set[int]
 
 
 class Sync(commands.Cog):
     def __init__(self, bot: SEBot):
         self.bot = bot
+        self.in_recovery = []
 
-    async def sync_user(self, target: discord.Member, start=False):
-        logger.debug(f'sync {target.name}')
-        guild: discord.Guild = target.guild
-        member_info = User_info(target.id)
-        saved_roles = User_roles.select(User_roles.role_id).where(
-            User_roles.user == target.id).dicts().execute()
+    def get_roles_change(self,
+                         target: discord.Member) -> tuple[outdated, added]:
+        member_info = Member_data_controller(target.id)
 
         user_roles_ids = [
             role.id for role in target.roles if role.name != '@everyone'
         ]
-        saved_roles_ids = [role['role_id'] for role in saved_roles]
+        saved_roles = member_info.user_info.user_roles
+        saved_roles_ids = [role.role_id for role in saved_roles]
 
-        to_save = set(user_roles_ids) - set(saved_roles_ids)
-        to_remove = set(saved_roles_ids) - set(user_roles_ids)
+        added = set(user_roles_ids) - set(saved_roles_ids)
+        outdated = set(saved_roles_ids) - set(user_roles_ids)
 
-        if start:
-            for role in to_save:
-                User_roles.get_or_create(user=target.id, role_id=role.id)
-            for role in to_remove:
-                role_in_db: User_roles = User_roles.get_or_none(
-                    user=target.id, role_id=role.id)
-                if role_in_db: role_in_db.delete_instance()
+        return outdated, added
 
-        else:
-            to_add = [
-                guild.get_role(role_id) for role_id in to_remove
-                if guild.get_role(role_id)
-            ]
-            to_remove = [
-                guild.get_role(role_id) for role_id in to_save
-                if guild.get_role(role_id)
-            ]
+    def update_saved_roles(self, target: discord.Member):
+        if target in self.in_recovery:
+            return
 
-            await target.add_roles(*to_add, reason='Recovery')
-            await target.remove_roles(*to_remove, reason='Recovery')
+        outdated, added = self.get_roles_change(target)
+        logger.debug(
+            f'update_saved_roles called for member {target}:\noutdated{outdated}\nadded{added}'
+        )
 
-        voice: Personal_voice = member_info.user_personal_voice
-        if not voice: return
-        if not guild.get_channel(voice['voice_id']):
+        for role in added:
+            User_roles.get_or_create(user=target.id, role_id=role)
+        for role in outdated:
+            role_in_db: User_roles = User_roles.get_or_none(user=target.id,
+                                                            role_id=role)
+            if role_in_db: role_in_db.delete_instance()
+
+    async def recovery_member_roles(self, target: discord.Member):
+        self.in_recovery.append(target)
+
+        outdated, added = self.get_roles_change(target)
+        logger.debug(
+            f'recovery_membet_roles called for member {target}:\noutdated{outdated}\nadded{added}'
+        )
+
+        outdated = [target.guild.get_role(role) for role in outdated]
+        added = [target.guild.get_role(role) for role in added]
+
+        await target.add_roles(*outdated, reason='Recovery')
+        await target.remove_roles(*added, reason='Recovery')
+
+        self.in_recovery.remove(target)
+
+    async def recovery_member_voice(self, target: discord.Member):
+        guild: discord.Guild = target.guild
+        member_info = Member_data_controller(target.id)
+
+        voice: Personal_voice = member_info.user_info.user_personal_voice
+        if len(voice) == 0: return
+        voice = voice[0]
+        if not guild.get_channel(voice.voice_id):
+            logger.debug(f'Recovery personal voice channel for {target.name}')
             category = get(guild.categories,
                            id=self.bot.config['personal_voice']['categoty'])
-            await guild.create_voice_channel(name=target.name,
-                                             category=category,
-                                             user_limit=voice['slots'])
+            new_voice = await guild.create_voice_channel(
+                name=target.name, category=category, user_limit=voice.slots)
+            voice.voice_id = new_voice.id
+            voice.save()
+
+    async def sync_user(self, target: discord.Member):
+        logger.debug(f'sync {target.name}')
+
+        await self.recovery_member_roles(target)
+        await self.recovery_member_voice(target)
+
+    def save_guild_info(self, guild: discord.Guild):
+        logger.debug(f'save_guild_info for guild {guild.name}')
+        members = guild.members
+        for member in members:
+            self.update_saved_roles(member)
 
     @commands.command()
     async def sync(self, ctx):
@@ -69,6 +103,37 @@ class Sync(commands.Cog):
             await ctx.tick(False)
         else:
             await ctx.tick(True)
+
+    @is_owner()
+    @commands.command()
+    async def init(self, ctx):
+        embed = DefaultEmbed(description =
+            'This action will overwrite the data for this guild.'\
+            +'Do not use if you are not sure you know the result.'
+        )
+        message = await ctx.confirm(embed=embed)
+        await message.delete()
+        try:
+            self.save_guild_info(ctx.guild)
+        except Exception as e:
+            logger.exception('Failed to save guild data')
+            await ctx.tick(False)
+        else:
+            await ctx.tick(True)
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member,
+                               after: discord.Member):
+        if before.roles != after.roles and len(after.roles) > 0:
+            self.update_saved_roles(after)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        await self.recovery_member_roles(member)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        self.save_guild_info(guild)
 
 
 def setup(bot):

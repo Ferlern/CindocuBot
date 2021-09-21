a2302a5d79326593b379d21232726f69bbb633c0
Ferlern <seeker1233@ukr.net>
2021-09-21 17:44:27 +0300

    prepare logger for prod.

--- core_elements/logger.py
+++ core_elements/logger.py
@@ -6,7 +6,11 @@ def create_logger(level):
     logger.setLevel(level=logging.getLevelName(level))
     handler = logging.StreamHandler()
     handler.setLevel(level=logging.getLevelName(level))
+    handler2 = logging.FileHandler('./core_elements/errors.log')
+    handler2.setLevel(logging.ERROR)
     logger.addHandler(handler)
+    logger.addHandler(handler2)
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
+    handler2.setFormatter(formatter)

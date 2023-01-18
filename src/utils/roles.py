ccdc89c24db1548179ebf6867254f8680744545c
Ferlern <67915420+Ferlern@users.noreply.github.com>
2023-01-18 14:09:08 +0200

    roles utils

---
+++ src/utils/roles.py
@@ -0,0 +1,34 @@
+from typing import Union, Sequence, Protocol
+import disnake
+
+
+class HasRoleId(Protocol):  # pylint: disable=too-few-public-methods
+    @property
+    def role_id(self) -> int:
+        ...
+
+
+def snowflake_roles_intersection(
+    *roles_like_seqs: Union[
+        Sequence[HasRoleId],
+        Sequence[disnake.abc.Snowflake],
+        Sequence[int],
+    ],
+) -> list[disnake.Object]:
+    result = set()
+    for roles_seq in roles_like_seqs:
+        if not roles_seq:
+            continue
+        if isinstance(roles_seq[0], int):
+            ids_set = set(roles_seq)
+        elif hasattr(roles_seq[0], 'role_id'):
+            ids_set = {role.role_id for role in roles_seq}  # type: ignore
+        elif isinstance(roles_seq[0], disnake.abc.Snowflake):
+            ids_set = {role.id for role in roles_seq}  # type: ignore
+        else:
+            raise TypeError('roles_like object must have id, role_id or be id himself')
+        if result:
+            result &= ids_set
+        else:
+            result = ids_set
+    return list(disnake.Object(role_id) for role_id in result)  # type: ignore

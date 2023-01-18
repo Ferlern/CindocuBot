b57a9a2b7c23c54f77e7a28974a3ed3eaf67fb79
Ferlern <67915420+Ferlern@users.noreply.github.com>
2023-01-18 14:08:51 +0200

    hex validator

--- src/utils/color.py
+++ src/utils/color.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 
 
@@ -7,3 +8,7 @@ class EmbedColors(int, Enum):
     RELATIONSHIPS = 0xffc0cb
     RELATIONSHIPS_ACCEPT = 0x9cee90
     RELATIONSHIPS_REFUSE = 0xe74c3c
+
+
+def validate_hex(hex_code: str) -> bool:
+    return bool(re.match(r'#(?:[0-9a-fA-F]{1,2}){3}$', hex_code))

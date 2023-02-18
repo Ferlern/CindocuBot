1206c40b3dc23dcaa80d90f7b986fd8582a59f9a
Ferlern <67915420+Ferlern@users.noreply.github.com>
2023-02-18 15:05:43 +0200

    add create_database function

--- src/database/create.py
+++ src/database/create.py
@@ -1,4 +1,7 @@
+import psycopg2
 from src.database.models import *
+from src.settings import DATABASE
+
 
 tables = (UserRoles, Likes, Members,
           Relationships, RelationshipParticipant,
@@ -7,6 +10,20 @@ tables = (UserRoles, Likes, Members,
           ExperienceSettings, PersonalVoice, Users, Guilds,
           History, PremoderationSettings, PremoderationItem,
           WelcomeSettings, ReminderSettings, Reminders, CreatedShopRoles, RolesInventory)
+
+
+def create_database() -> None:
+    connection = psycopg2.connect(
+        host=DATABASE['host'],
+        port=DATABASE['port'],
+        user=DATABASE['user'],
+        password=DATABASE['password']
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s;", (DATABASE['dbname'],))
+    exists = cursor.fetchone()
+    if not exists:
+        cursor.execute('CREATE DATABASE %s;', (DATABASE['dbname'],))
 
 
 def create_tables() -> None:

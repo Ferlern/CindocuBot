e7abcf0477366eaa2abde0145994b586e3ed5e07
Сергей Трофимов <sm.trofimov2004@gmail.com>
2024-06-29 17:43:22 +0300

    fixed issue

--- src/ext/game/views/game_interfaces/__init__.py
+++ src/ext/game/views/game_interfaces/__init__.py
@@ -1,6 +1,5 @@
 from .base import DiscordInterface
 from .dice import DiceDiscordInterface
-from .secrets.secrets import SecretsDiscordInterface
 from .channel_base import ChannelGameInterface
 from .bunker.bunker import BunkerDiscordInterface
 
@@ -8,7 +7,6 @@ from .bunker.bunker import BunkerDiscordInterface
 __all__ = (
     'DiscordInterface',
     'DiceDiscordInterface',
-    'SecretsDiscordInterface',
     'ChannelGameInterface',
     'BunkerDiscordInterface',
 )

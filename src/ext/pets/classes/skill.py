74c85daf41ab6c60107a4d78bd987b80933c3a1d
Сергей Трофимов <sm.trofimov2004@gmail.com>
2024-09-04 10:53:46 +0300

    cooldown update

--- src/ext/pets/classes/skill.py
+++ src/ext/pets/classes/skill.py
@@ -161,7 +161,7 @@ class Rage(Skill):
             id = "s3",
             name = "Rage",
             affects = {"in_rage": 2},
-            cooldown = 2
+            cooldown = 3
         )
 
         self._journal_desc = t("rage_journal_desc")

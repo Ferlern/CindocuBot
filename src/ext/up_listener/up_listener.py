6705e07a3fd610bd9d3dfc2c2c015592fe4d5e56
Toshi711 <romakozhevnik@mail.ru>
2026-07-21 23:00:34 +0500

    feat: up/like reminders

--- src/ext/up_listener/up_listener.py
+++ src/ext/up_listener/up_listener.py
@@ -1,22 +1,28 @@
+import types
+
 import disnake
 from disnake.ext import commands
 
+from src.bot import SEBot
 from src.discord_views.embeds import DefaultEmbed
 from src.ext.economy.services import change_balance
-from src.translation import get_translator
-from src.bot import SEBot
 from src.logger import get_logger
+from src.translation import get_translator
 from src.utils import custom_events
-
 
 logger = get_logger()
 t = get_translator(route='ext.up_listener')
-UP_MESSAGES_CHECKS = {
-    464272403766444044: lambda embed: embed.color.value == 4437377,
-    575776004233232386: lambda embed: embed.description.startswith((
-        'Вы успешно лайкнули', 'You successfully liked')),  # noqa
-}
 
+UP_MONITORING_ID = 1244278183814238259
+LIKE_MONITORING_ID = 1135447052734709761
+
+def _is_up(embed): return embed.color is not None and embed.color.value == 4437377
+def _is_like(embed): return bool(embed.description) and embed.description.startswith(('Вы успешно лайкнули', 'You successfully liked'))
+
+UP_MESSAGES_CHECKS = {
+  UP_MONITORING_ID: lambda e: UP_MONITORING_ID if _is_up(e) else None,
+  LIKE_MONITORING_ID: lambda e: LIKE_MONITORING_ID if _is_like(e) else None,
+}
 
 class UpListenerCog(commands.Cog):
     def __init__(self, bot: SEBot) -> None:
@@ -32,36 +38,39 @@ class UpListenerCog(commands.Cog):
         await self._check_for_up(after)
 
     async def _check_for_up(self, message: disnake.Message) -> None:
+        
         if message.id in self._m:
-            return
-        check = UP_MESSAGES_CHECKS.get(message.author.id)
-        if not check:
-            return
+          return
+        
+        resolve = UP_MESSAGES_CHECKS.get(message.author.id)
 
-        # if message.author.id == 464272403766444044:
-        #     await asyncio.sleep(4)
-        #     message = await message.channel.fetch_message(message.id)
-
+        if not resolve:
+            return
+        
         if not message.embeds or not message.guild or not message.interaction:
             logger.info('Message from %d, but no embeds / guild / interaction', message.author.id)
             return
 
-        if not check(message.embeds[0]):
+        monitoring_id = resolve(message.embeds[0])
+
+        if monitoring_id is None:
             logger.info('Message from %d, but embed check is not passed', message.author.id)
             return
 
         self._m.append(message.id)
         self._m = self._m[-10:]
 
+
         change_balance(
             guild_id=message.guild.id,
             user_id=message.interaction.author.id,
             amount=25,
         )
+
         self.bot.dispatch(
             custom_events.EventName.MONITORING_GUILD_PROMOTED.value,
             message.guild,
-            message.author,
+            types.SimpleNamespace(id=monitoring_id),
         )
 
         await message.channel.send(reference=message, embed=DefaultEmbed(
--- src/ext/up_listener/up_reminder.py
+++ src/ext/up_listener/up_reminder.py
@@ -1,86 +1,96 @@
 from __future__ import annotations
+
 import asyncio
-from typing import NamedTuple, Optional, TYPE_CHECKING
 import datetime
+from typing import TYPE_CHECKING, NamedTuple, Optional
 
 import disnake
-from disnake.ext import commands
+from disnake.ext import commands, tasks
 from pytz import timezone
 from pytz.tzinfo import BaseTzInfo
 
-from src.translation import get_translator
+from src.ext.up_listener.services import (
+    create_or_overrite_old_reminder,
+    get_all_active_not_outdated_reminders,
+    get_all_reminder_settings,
+    get_reminder_settings,
+)
 from src.logger import get_logger
+from src.translation import get_translator
 from src.utils import custom_events
-from src.ext.up_listener.services import (get_reminder_settings,
-                                          create_or_overrite_old_reminder,
-                                          get_all_active_not_outdated_reminders)
+
 if TYPE_CHECKING:
     from src.bot import SEBot
 
-
 logger = get_logger()
 t = get_translator(route='ext.up_listener')
-
-
 class MonitoringData(NamedTuple):
     reset_days: tuple[int, ...]
     reset_time: int
-    cooldown: int
+    cooldown: datetime.timedelta
     timezone: BaseTzInfo
 
-
 MONITORING_INFORMATION = {
-    464272403766444044: MonitoringData(
+    1244278183814238259: MonitoringData(
         reset_days=(1, 15),
         reset_time=12,
-        cooldown=4,
+        cooldown=datetime.timedelta(hours=4),
         timezone=timezone('Europe/Moscow'),
     ),
-    575776004233232386: MonitoringData(
+    1135447052734709761: MonitoringData(
         reset_days=(1, 15),
-        reset_time=3,
-        cooldown=4,
-        timezone=timezone('Europe/Moscow'),
+        reset_time=0,
+        cooldown=datetime.timedelta(hours=4),
+        timezone=timezone('GMT'),
     )
 }
 
+REMINDER_LEAD = datetime.timedelta(seconds=35)
 
 class UpReminderCog(commands.Cog):
     def __init__(self, bot: SEBot) -> None:
         self.bot = bot
+        self._active: set[tuple[int, int, datetime.datetime]] = set()
+        self.reset_scheduler.start()
+
+    def cog_unload(self) -> None:
+        self.reset_scheduler.cancel()
 
     @commands.Cog.listener(f'on_{custom_events.EventName.MONITORING_GUILD_PROMOTED.value}')
     async def up_listener(self, guild: disnake.Guild, monitoring_bot: disnake.User) -> None:
+
         info = MONITORING_INFORMATION.get(monitoring_bot.id)
         if not info:
             return
 
         settings = get_reminder_settings(guild.id, monitoring_bot.id)
+
         channel = guild.get_channel(settings.channel_id)  # type: ignore
+
         if not self.check_reminder(
             channel,
             text=settings.text  # type: ignore
         ):
             return
 
-        logger.info('Creating remidner for guild %d, monitoring %d',
-                    guild.id, monitoring_bot.id)
+        logger.info('Creating remidner for guild %d, monitoring %d', guild.id, monitoring_bot.id)
         current_time = datetime.datetime.now(info.timezone)
-        if not is_close_to_reset(info):
-            send_time = current_time + datetime.timedelta(hours=info.cooldown, seconds=-30)
-        else:
-            send_time = info.timezone.localize(datetime.datetime(
-                current_time.year,
-                current_time.month,
-                current_time.day,
-                info.reset_time,
-            ))
+        close_to_reset = is_close_to_reset(info)
 
-        create_or_overrite_old_reminder(guild.id, monitoring_bot.id, send_time)
-        await self.send_reminder(
+        if close_to_reset:
+            send_time = next_reset(info) - REMINDER_LEAD
+        else:
+            send_time = current_time + info.cooldown - REMINDER_LEAD
+
+        create_or_overrite_old_reminder(
+            guild.id, monitoring_bot.id, send_time, force=close_to_reset,
+        )
+
+        self._launch_reminder(
+            guild.id, monitoring_bot.id,
             channel,  # type: ignore
             settings.text,  # type: ignore
-            send_time
+            send_time,
         )
 
     async def load_reminders(self) -> None:
@@ -95,21 +105,15 @@ class UpReminderCog(commands.Cog):
             settings = get_reminder_settings(guild.id, reminder.monitoring_bot_id)
             channel = guild.get_channel(settings.channel_id)  # type: ignore
             if self.check_reminder(channel, settings.text):  # type: ignore
-                logger.info('Creating remidner for guild %d, monitoring %d',
+                logger.info('Restoring reminder for guild %d, monitoring %d',
                             guild.id, reminder.monitoring_bot_id)
-                task = self.send_reminder(channel, settings.text, reminder.send_time)  # type: ignore
-                self.bot.loop.create_task(task)
+                self._launch_reminder(
+                    guild.id, reminder.monitoring_bot_id,
+                    channel, settings.text, reminder.send_time,  # type: ignore
+                )
 
-    def check_reminder(
-        self,
-        channel: Optional[disnake.abc.GuildChannel],
-        text: Optional[str],
-    ) -> bool:
-        if not channel or not isinstance(channel, disnake.TextChannel):
-            return False
-        if not text:
-            return False
-        return True
+    def check_reminder(self, channel: Optional[disnake.abc.GuildChannel], text: Optional[str]) -> bool:
+        return bool(channel and isinstance(channel, disnake.TextChannel) and text)
 
     async def send_reminder(
         self,
@@ -117,6 +121,7 @@ class UpReminderCog(commands.Cog):
         text: str,
         send_time: datetime.datetime
     ) -> None:
+        
         current_time = datetime.datetime.now().astimezone()
         wait_time = (send_time - current_time).total_seconds()
         logger.debug('send_reminder will sleep for %d second', wait_time)
@@ -132,16 +137,99 @@ class UpReminderCog(commands.Cog):
             ),
         )
 
+    def _launch_reminder(
+        self,
+        guild_id: int,
+        monitoring_bot_id: int,
+        channel: disnake.TextChannel,
+        text: str,
+        send_time: datetime.datetime,
+    ) -> None:
+        key = (guild_id, monitoring_bot_id, send_time)
+        
+        if key in self._active:
+            logger.debug('Reminder %s already scheduled, skip', key)
+            return
+        
+        self._active.add(key)
+        self.bot.loop.create_task(self._run_reminder(key, channel, text, send_time))
+
+    async def _run_reminder(
+        self,
+        key: tuple[int, int, datetime.datetime],
+        channel: disnake.TextChannel,
+        text: str,
+        send_time: datetime.datetime,
+    ) -> None:
+        try:
+            await self.send_reminder(channel, text, send_time)
+        finally:
+            self._active.discard(key)
+
+    @tasks.loop(seconds=60)
+    async def reset_scheduler(self) -> None:
+        for monitoring_bot_id, info in MONITORING_INFORMATION.items():
+            if not is_close_to_reset(info):
+                continue
+            
+            send_time = next_reset(info) - REMINDER_LEAD
+
+            if send_time <= datetime.datetime.now(info.timezone):
+                continue
+            
+            for settings in get_all_reminder_settings(monitoring_bot_id):
+                guild = self.bot.get_guild(settings.guild_id.id)  # type: ignore
+                
+                if not guild:
+                    continue
+                
+                channel = guild.get_channel(settings.channel_id)  # type: ignore
+                
+                if not self.check_reminder(channel, settings.text):  # type: ignore
+                    continue
+                
+                if (guild.id, monitoring_bot_id, send_time) in self._active:
+                    continue
+                
+                logger.info('Reset scheduler arms reminder for guild %d, monitoring %d at %s', guild.id, monitoring_bot_id, send_time.isoformat())
+                
+                create_or_overrite_old_reminder(
+                    guild.id, monitoring_bot_id, send_time, force=True,
+                )
+
+                self._launch_reminder(
+                    guild.id, monitoring_bot_id,
+                    channel, settings.text, send_time,  # type: ignore
+                )
+
+    @reset_scheduler.before_loop
+    async def before_reset_scheduler(self) -> None:
+        await self.bot.wait_until_ready()
+
+
+def next_reset(info: MonitoringData) -> datetime.datetime:
+    now = datetime.datetime.now(info.timezone)
+    candidates = []
+
+    for month_offset in (0, 1):
+        month_index = now.month - 1 + month_offset
+        year = now.year + month_index // 12
+        month = month_index % 12 + 1
+
+        for day in info.reset_days:
+            moment = info.timezone.localize(datetime.datetime(
+                year, month, day, info.reset_time
+            ))
+            
+            if moment > now:
+                candidates.append(moment)
+
+    return min(candidates)
+
 
 def is_close_to_reset(info: MonitoringData) -> bool:
-    current_time = datetime.datetime.now(info.timezone)
-    for day in info.reset_days:
-        reset_time = current_time.replace(day=day, hour=info.reset_time, minute=0, second=0)
-        delta = reset_time - current_time
-        if datetime.timedelta() < delta < datetime.timedelta(hours=info.cooldown):
-            return True
-    return False
-
+    now = datetime.datetime.now(info.timezone)
+    return next_reset(info) - now < info.cooldown
 
 def setup(bot) -> None:
     cog = UpReminderCog(bot)
